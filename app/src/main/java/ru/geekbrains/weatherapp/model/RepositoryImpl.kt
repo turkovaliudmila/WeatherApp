6f49432c3f1c7b7c03ed1da1ddd174c916efde59
turkovaliudmila <ljudmila327@rambler.ru>
2021-05-03 20:49:44 +0300

    Добавлены классы слоя Model

---
+++ app/src/main/java/ru/geekbrains/weatherapp/model/RepositoryImpl.kt
@@ -0,0 +1,12 @@
+package ru.geekbrains.weatherapp.model
+
+class RepositoryImpl : Repository {
+
+    override fun getWeatherFromServer(): Weather {
+        return Weather()
+    }
+
+    override fun getWeatherFromLocalStorage(): Weather {
+        return Weather()
+    }
+}
