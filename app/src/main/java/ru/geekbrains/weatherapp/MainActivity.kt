352ba237dbb1f48fbfa11f3403a4abb59afcec33
turkovaliudmila <ljudmila327@rambler.ru>
2021-05-03 20:25:49 +0300

    Добавлен макет фрагмента для основного экрана

--- app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
@@ -2,17 +2,21 @@ package ru.geekbrains.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.geekbrains.weatherapp.databinding.MainActivityBinding
+import ru.geekbrains.weatherapp.databinding.MainFragmentBinding
 import ru.geekbrains.weatherapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(getLayoutInflater())
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
     }
 }
--- app/src/main/java/ru/geekbrains/weatherapp/ui/main/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/ui/main/MainFragment.kt
@@ -7,8 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.geekbrains.weatherapp.R
+import ru.geekbrains.weatherapp.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
 
     companion object {
         fun newInstance() = MainFragment()
@@ -18,13 +23,19 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
