1a83e4da6d9a2c6b06e7db30f710d3dcb071fe2f
turkovaliudmila <ljudmila327@rambler.ru>
2021-05-03 21:02:31 +0300

    Реализован слой ViewModel и его взаимодействие со основным фрагментом

--- app/src/main/java/ru/geekbrains/weatherapp/ui/main/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/ui/main/MainFragment.kt
@@ -8,12 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import ru.geekbrains.weatherapp.R
 import ru.geekbrains.weatherapp.databinding.MainFragmentBinding
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import ru.geekbrains.weatherapp.model.AppState
+import ru.geekbrains.weatherapp.model.Weather
 
 class MainFragment : Fragment() {
 
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
-
 
     companion object {
         fun newInstance() = MainFragment()
@@ -30,6 +33,37 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderData(it) })
+        viewModel.getWeatherFromLocalSource()
+
+    }
+
+    private fun renderData(appState: AppState) {
+        when (appState) {
+            is AppState.Success -> {
+                val wheatherData = appState.weatherData
+                binding.loadingLayout.visibility = View.GONE;
+                setData(wheatherData)
+                Snackbar.make(binding.mainView, "Success", Snackbar.LENGTH_LONG).show()
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE;
+            }
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE;
+                Snackbar
+                    .make(binding.mainView, "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") { viewModel.getWeatherFromLocalSource() }
+                    .show()
+            }
+        }
+    }
+
+    private fun setData(weatherData: Weather) {
+        binding.cityName.text = weatherData.City.city
+        binding.cityCoordinates.text = String.format(getString(R.string.city_coordinates), weatherData.City.lat.toString(), weatherData.City.lon.toString())
+        binding.temperatureValue.text = weatherData.temperature.toString()
+        binding.feelsLikeValue.text = weatherData.feelsLike.toString()
 
     }
 
--- app/src/main/java/ru/geekbrains/weatherapp/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/ui/main/MainViewModel.kt
@@ -1,7 +1,25 @@
 package ru.geekbrains.weatherapp.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.geekbrains.weatherapp.model.AppState
+import ru.geekbrains.weatherapp.model.Repository
+import ru.geekbrains.weatherapp.model.RepositoryImpl
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+                    private val repository: Repository = RepositoryImpl()) : ViewModel() {
+
+    fun getLiveData() = liveDataToObserve
+
+    fun getWeatherFromLocalSource() = getDataFromLocalSource()
+
+    fun getWeatherFromRemoteSource() = getDataFromLocalSource()
+
+    private fun getDataFromLocalSource() {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            Thread.sleep(1000)
+            liveDataToObserve.postValue(AppState.Success(repository.getWeatherFromLocalStorage()))
+        }.start()
+    }
 }
